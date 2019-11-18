8c21d89b5755ecdf69b1e52a6ab95627cec50e67
unknown <whgusrjf33@naver.com>
2019-11-18 15:44:20 +0800

    TxHash 정보 펜딩 처리

--- routes/tx.js
+++ routes/tx.js
@@ -6,22 +6,32 @@ const addComma = require('../public/js/addComma');
 //var Window = document.defaultView;
 //document.defaultView = value;
 
-router.get('/:pageId', function (req, res) {
+router.get('/:pageId', async function (req, res) {
     let pageId = req.params.pageId;
-    web3.eth.getTransaction(pageId, false, function (err, tx) {
+    await web3.eth.getTransaction(pageId, false, async function (err, tx) {
         if(tx === null || err){
             return res.render('error')
         }
-        web3.eth.getTransactionReceipt(pageId, false, async function (err, txReceipt) {
+        if(!tx.blockHash) {
+            let value = web3.utils.fromWei(tx.value, 'ether');
+            let price = web3.utils.fromWei(tx.gasPrice, 'ether');
+
+            return res.render('tx', {
+                hash: tx.hash, status: 'Pending', blockNumber: 'Pending', from: tx.from, to: tx.to, value: value,
+                gasUsed: 'Pending', gasPrice: price, nonce: tx.nonce, timestamp: 'Pending', txFee: 'Pending', input: tx.input
+            })
+        }
+        await web3.eth.getTransactionReceipt(pageId, false, async function (err, txReceipt) {
             let value = web3.utils.fromWei(tx.value, 'ether');
             let price = web3.utils.fromWei(tx.gasPrice, 'ether');
             let txFee = web3.utils.fromWei((tx.gasPrice * txReceipt.gasUsed).toString(10))
-          
-            web3.eth.getBlock(tx.blockNumber, false, function (err, block) {
+            let status;
+      
+            await web3.eth.getBlock(tx.blockNumber, false, function (err, block) {
                 let timestamp = block.timestamp * 1000;
                 let date = new Date(timestamp);
                 return res.render('tx', {
-                    hash: tx.hash, status: txReceipt.status, blockNumber: tx.blockNumber, from: tx.from, to: tx.to, value: value,
+                    hash: tx.hash, status: status, blockNumber: tx.blockNumber, from: tx.from, to: tx.to, value: value,
                     gasUsed: addComma(txReceipt.gasUsed), gasPrice: price, nonce: tx.nonce, timestamp: date, txFee: txFee, input: tx.input
                 })
             })
